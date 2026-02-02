a47a374309777383edf114467ac6782ba8d9fd35
igorzizinio <igorschug6@gmail.com>
2026-02-02 09:17:44 -0300

    add simple terminal message

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,10 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+const APP_NAME: &str = env!("CARGO_PKG_NAME");
+const APP_VERSION: &str = env!("CARGO_PKG_VERSION");
+
 fn main() {
+    println!("{} v{}", APP_NAME, APP_VERSION);
     unhexo_lib::run()
 }
